c68f1a7c9acdf543caf0dfe919a352005f99a63b
Romina Vargas <rsocci@me.com>
2013-10-11 13:41:02 -0400

    Make both queries identical for same method use

--- app/models/stats/tag_cloud.rb
+++ app/models/stats/tag_cloud.rb
@@ -11,7 +11,8 @@ class TagCloud
   def compute
     levels=10
 
-    @tags = Tag.find_by_sql(sql).sort_by { |tag| tag.name.downcase }
+    params = [sql, user.id]
+    @tags = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
 
     max, @tags_min = 0, 0
     @tags.each { |t|
@@ -21,9 +22,8 @@ class TagCloud
 
     @tags_divisor = ((max - @tags_min) / levels) + 1
 
-    @tags_for_cloud_90days = Tag.find_by_sql(
-      [sql_90days, user.id, @cut_off, @cut_off]
-    ).sort_by { |tag| tag.name.downcase }
+    params = [sql(@cut_off), user.id, @cut_off, @cut_off]
+    @tags_for_cloud_90days = Tag.find_by_sql(params).sort_by { |tag| tag.name.downcase }
 
     max_90days, @tags_min_90days = 0, 0
     @tags_for_cloud_90days.each { |t|
@@ -36,30 +36,19 @@ class TagCloud
 
   private
 
-  def sql_90days
+  def sql(cut_off = nil)
     query = "SELECT tags.id, tags.name AS name, count(*) AS count"
     query << " FROM taggings, tags, todos"
     query << " WHERE tags.id = tag_id"
     query << " AND todos.user_id=? "
     query << " AND taggings.taggable_type='Todo' "
     query << " AND taggings.taggable_id=todos.id "
-    query << " AND (todos.created_at > ? OR "
-    query << "      todos.completed_at > ?) "
+    if cut_off
+      query << " AND (todos.created_at > ? OR "
+      query << "      todos.completed_at > ?) "
+    end
     query << " GROUP BY tags.id, tags.name"
     query << " ORDER BY count DESC, name"
     query << " LIMIT 100"
-  end
-
-  def sql
-    query = "SELECT tags.id, name, count(*) AS count"
-    query << " FROM taggings, tags, todos"
-    query << " WHERE tags.id = tag_id"
-    query << " AND taggings.taggable_id = todos.id"
-    query << " AND todos.user_id="+user.id.to_s+" "
-    query << " AND taggings.taggable_type='Todo' "
-    query << " GROUP BY tags.id, tags.name"
-    query << " ORDER BY count DESC, name"
-    query << " LIMIT 100"
-
   end
 end
